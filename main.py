8f9d04902470529c3fd7a550059e345295cbf19b
Taner Süzen <taner.suzen95@gmail.com>
2023-11-25 14:06:00 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import turtle
+from turtle import Turtle
+from random import random
+import time
+
+game_board = turtle.Screen()
+game_board.bgcolor("#cbbeb5")
+
+catch_turtle = Turtle()
+catch_turtle.shape("turtle")
+catch_turtle.color("green")
+catch_turtle.shapesize(1.3)
+
+
+catch_turtle.screen.mainloop()
