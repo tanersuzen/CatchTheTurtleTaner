2ab5d8df27d791453fdb277132a51c489380294a
Taner Süzen <taner.suzen95@gmail.com>
2023-11-25 15:54:33 +0300

    revision1

--- main.py
+++ main.py
@@ -11,5 +11,23 @@ catch_turtle.shape("turtle")
 catch_turtle.color("green")
 catch_turtle.shapesize(1.3)
 
+my_time = 20
+
+def counterclock():
+    turtle.write(f"Time: {my_time}", align="center", font=("Arial", 20, "normal"))
+
+for x in range(my_time,0,-1):
+    counterclock()
+    time.sleep(1)
+    my_time = my_time -1
+    turtle.clear()
+
+def endgame():
+    turtle.write("Game is Over",align="center",font=("Arial",20,"normal"))
+
+if my_time == 0:
+    endgame()
+
+
 
 catch_turtle.screen.mainloop()
