15cc604f9a98515227ff2222c33bd2ccac38a5f1
Taner Süzen <taner.suzen95@gmail.com>
2023-11-26 13:40:49 +0300

    revision4

--- main.py
+++ main.py
@@ -6,55 +6,70 @@ import time
 game_board = turtle.Screen()
 game_board.bgcolor("#cbbeb5")
 my_time = 20
-
-catch_turtle = turtle.Turtle()
-catch_turtle.shape("turtle")
-catch_turtle.color("green")
-catch_turtle.shapesize(3,3)
-catch_turtle.penup()
-catch_turtle.speed(0)
-
-
-clock = Turtle()
-clock.penup()
-clock.hideturtle()
-clock.setposition(0, 270)
-
-score = Turtle()
-score.color("blue")
-score.penup()
-score.hideturtle()
-score.setposition(0,300)
 scorenum = 0
+game_over = False
+catch_turtle = turtle.Turtle()
+clock = Turtle()
+score = Turtle()
 
-def handclick(a,b):
-    global scorenum
-    scorenum += 1
-    score.clear()
-    score.write(f"Score: {scorenum}", align= "center", font=("Arial",24,"normal"))
+def catching():
+    catch_turtle.clear()
+    catch_turtle.shape("turtle")
+    catch_turtle.color("green")
+    catch_turtle.shapesize(3, 3)
+    catch_turtle.penup()
+    catch_turtle.speed(0)
+    if not game_over:
+        catch_turtle.onclick(handclick)
+        positionx = random.randint(-300, 300)
+        positiony = random.randint(-300, 300)
+        catch_turtle.setposition(positionx, positiony)
+        game_board.ontimer(catching,620)
+    else:
+        catch_turtle.hideturtle()
 
-catch_turtle.onclick(handclick)
-
-def mainfunc():
-    my_time = 20
-    score.write (f"Score: {scorenum}", align= "center", font=("Arial",24,"normal"))
-    for x in range(my_time, 0, -1):
-        clock.write(f"Time: {my_time}", align="center", font=("Arial", 20, "normal"))
-        my_time = my_time - 1
-        for el in range (0,1):
-            positionx = random.randint(-300, 300)
-            positiony = random.randint(-300, 300)
-            catch_turtle.setposition(positionx, positiony)
-            time.sleep(0.5)
+def timer(my_time):
+    global game_over
+    clock.penup()
+    clock.hideturtle()
+    clock.setposition(0, 270)
+    clock.clear()
+    if my_time > 0:
         clock.clear()
-    if my_time == 0:
+        clock.hideturtle()
+        clock.write(f"Time: {my_time}", align="center", font=("Arial", 20, "normal"))
+        game_board.ontimer(lambda: timer(my_time-1),1000)
+    else:
+        game_over = True
+        clock.clear()
+        clock.write("Game is Over!", align="center", font=("Arial", 20, "normal"))
         endgame = Turtle()
         endgame.penup()
         endgame.hideturtle()
         endgame.setposition(0, 0)
         endgame.write(f"Final Score: {scorenum}", align="center", font=("Arial", 20, "normal"))
-        catch_turtle.onclick(None)
 
-mainfunc()
+def scoring():
+    score.color("dark blue")
+    score.penup()
+    score.hideturtle()
+    score.setposition(0,300)
+    global scorenum
+    score.write(f"Score: {scorenum}", align="center", font=("Arial", 24, "normal"))
 
-game_board.mainloop()
+def handclick(x,y):
+    global scorenum
+    scorenum += 1
+    score.clear()
+    score.write(f"Score: {scorenum}", align= "center", font=("Arial",24,"normal"))
+
+def gamestartup():
+    scoring()
+    timer(my_time)
+    catching()
+
+
+
+gamestartup()
+
+turtle.mainloop()
